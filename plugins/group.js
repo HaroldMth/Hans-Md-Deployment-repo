4bf5afae52db1f7bfc87981928470747e8f9dcc8
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-22 11:07:02 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/group.js
+++ plugins/group.js
@@ -293,6 +293,44 @@ cmd({
             }
         }
     )*/
+
+//---------------------------------------------------------------------------
+cmd({
+    pattern: "setwelcome",
+    desc: "sets welcome message in specific group.",
+    category: "group",
+},
+async(Void, citel, text,{ isCreator }) => {
+    if (!isCreator) return citel.reply(tlang().owner)
+          let Group = await sck.findOne({ id: citel.chat })
+            if (!Group) {
+                await new sck({ id: citel.chat, welcome: text,events:'true' }).save()
+                return citel.reply('Welcome added added for this group.')
+            } else {
+                await await sck.updateOne({ id: citel.chat }, { welcome:text ,events:'true'})
+                return citel.reply('Welcome msg has been updated successfully.')
+                
+            }      
+}
+)
+ //---------------------------------------------------------------------------
+cmd({
+    pattern: "setgoodbye",
+    desc: "sets goodbye message in specific group.",
+    category: "group",
+},
+async(Void, citel, text,{ isCreator }) => {
+    if (!isCreator) return citel.reply(tlang().owner)
+          let Group = await sck.findOne({ id: citel.chat })
+            if (!Group) {
+                await new sck({ id: citel.chat, goodbye: text,events:'true' }).save()
+                return citel.reply('Goodbye added for this group.');
+            } else {
+                await await sck.updateOne({ id: citel.chat }, { goodbye:text,events:'true' })
+                return citel.reply('Goodbye msg has been updated successfully.');     
+            }      
+}
+)
 //---------------------------------------------------------------------------
 cmd({
         pattern: "kik",
