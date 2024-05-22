c7a47c476df61c4e9c6bf2c63519c247bf5e03b1
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-22 10:24:41 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/general.js
+++ plugins/general.js
@@ -267,52 +267,42 @@ ${cpus[0] ? `  *❲❍❳ Total CPU Usage:*
 
 //---------------------------------------------------------------------------
 cmd({
-  pattern: "ping2",
-  desc: "To check bot status",
+  pattern: "calc",
+  desc: "a Simple Calculator For Maths",
   category: "general",
   filename: __filename,
-},
-async(darkshan, msg) => {
-  try{
-      const used = process.memoryUsage()
-      const cpus = os.cpus().map(cpu => {
-          cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-          return cpu
-      })
-      const cpu = cpus.reduce((last, cpu, _, { length }) => 
-      {
+}, (Void, citel, text) => {
+  const parts = text.split(' ');
+  if (parts.length !== 3) {
+    return citel.reply('Usage: !calc <num1> <operator> <num2>');
+  }
+  const num1 = parseFloat(parts[0]);
+  const operator = parts[1];
+  const num2 = parseFloat(parts[2]);
+  if (isNaN(num1) || isNaN(num2)) {
+    return citel.reply('Please provide valid numerical values.');
+  }
 
-          last.total += cpu.total
-          last.speed += cpu.speed / length
-          last.times.user += cpu.times.user
-          last.times.nice += cpu.times.nice
-          last.times.sys += cpu.times.sys
-          last.times.idle += cpu.times.idle
-          last.times.irq += cpu.times.irq
-          return last
-      },{ speed: 0,total: 0,times: {user: 0,nice: 0,sys: 0,idle: 0,irq: 0 } }
-      )
-    timestampe = speed();
-    latensie = speed() - timestampe;
-    var neww = performance.now()
-    var oldd = performance.now()
-                  
-    Response Speed: ${latensie.toFixed(3)}/${(oldd - neww).toFixed(3)} _Second_ 
- 0.008625000715255737 _miliseconds_
-    
-  Runtime: ${runtime(process.uptime())}
-  _💻Server Info_
-  RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
-  
-  Nodjs Memory Usage:
-      ${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n      ')}
-  
-${cpus[0] ? `  _Total CPU Usage_
-  *${cpus[0].model.trim()} (${cpu.speed} MHZ)*
-      ${Object.keys(cpu.times).map(type => `-${(type + '').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n      ')}
-\n End 
-})
+  let result;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      return citel.reply('Invalid operator. Supported operators are +, -, *, and /.');
+  }
 
+  citel.reply(`Result: ${result}`);
+});
 
 //---------------------------------------------------------------------------
 cmd({
