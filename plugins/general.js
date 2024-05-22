6ab4ad20722c2a37c254df25b3fef91a8810023f
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-22 08:51:51 +0500

    Update general.js

--- plugins/general.js
+++ plugins/general.js
@@ -229,3 +229,17 @@ return citel.reply(str)
     
 }
 )
+
+     //---------------------------------------------------------------------------
+ cmd({ on: "body" }, async(Void, citel) => {
+     if (Config.autoreaction === 'true') {
+         const emojis = ['❤', '💕', '😻', '🧡', '💛', '💚', '💙', '💜', '🖤', '❣', '💞', '💓', '💗', '💖', '💘', '💝', '💟', '♥', '💌', '🙂', '🤗', '😌', '😉', '🤗', '😊', '🎊', '🎉', '🎁', '🎈', '👋']
+         const emokis = emojis[Math.floor(Math.random() * (emojis.length))]
+         Void.sendMessage(citel.chat, {
+             react: {
+                 text: emokis,
+                 key: citel.key
+             }
+         })
+     }
+ })
