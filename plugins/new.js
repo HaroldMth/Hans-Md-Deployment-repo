732b0ab0e9645e8c2a56c529c914dd64d463a16c
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-23 10:42:47 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/new.js
+++ plugins/new.js
@@ -384,6 +384,45 @@ cmd({
       }
 
 })
+
+ //-------------------------------------------------------------
+let recordedMessage = '';
+
+cmd({
+   pattern: "setaza",
+   desc: "Store a message as account number",
+  fromMe:true,
+   category: "user",
+ }, async (Void, citel, text) => {
+   // Check if a message is already recorded
+   if (recordedMessage === '') {
+     const message = text.trim();
+     recordedMessage = message;
+     await citel.reply(`Account number recorded: "${message}"`);
+   } else {
+     await citel.reply("A message is already recorded.");
+   }
+ });
+
+ //-------------------------------------------------------------
+cmd({
+   pattern: "delaza",
+   desc: "Delete the recorded account number",
+   category: "user",
+  fromme:true
+ }, async (Void, citel) => {
+   recordedMessage = '';
+   await citel.reply("Account number deleted.");
+ });
+
+ cmd({
+   on: "text",
+  fromMe:false,
+ }, async (Void, citel, text) => {
+   if (/(\baza\b|\bsend aza\b|\baccount number\b)/i.test(text) && recordedMessage) {
+     await citel.reply(recordedMessage);
+   }
+ });
 //---------------------------------------------------------------------------    
 
 cmd({
