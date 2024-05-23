77575b6d083df35f96d02fb34cd7969349c3c381
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-23 20:00:44 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/new.js
+++ plugins/new.js
@@ -3,6 +3,8 @@ const { mediafire } = require("../lib/mediafire.js");
 const googleTTS = require("google-tts-api");
 const ytdl = require('ytdl-secktor')
 const fs = require('fs-extra')
+const util = require('util');
+const axios = require('axios');
 const moment = require("moment-timezone");
 const Levels = require("discord-xp");
 const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
@@ -424,6 +426,20 @@ cmd({
  });
 
  //-------------------------------------------------------------
+cmd({
+        pattern: "paste",
+        desc: "create paste of text.",
+        category: "extra",
+        filename: __filename,
+    },
+    async(Void, citel,text) => {
+ let a = citel.quoted ? citel.quoted.text : citel.text;
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-2.0+Bot&author_name=Slasher-Official &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
+    }
+);
+
+ //-------------------------------------------------------------
 cmd(
     {
         pattern: "wamod", // Change the command pattern to something you prefer
@@ -759,7 +775,7 @@ cmd({
 cmd({
 
             pattern: "support",           
-            alias :['supp','wasupp','ownersupp'],
+            alias :['supp','wasupp','channel'],
             desc: "(menu cmdlist).",
             category: "user",
             react: "😏",
