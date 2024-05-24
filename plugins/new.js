378f5d304591c17ec27e9c7e65d13706b65ae28a
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-24 13:46:31 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/new.js
+++ plugins/new.js
@@ -20,33 +20,7 @@ const rapidApiKey = 'bcdeae8e6bmsh3af33e24439971ep106cd9jsnfc28157e482b';
 
 //---------------------------------------------------------------------------
 cmd({
-    pattern: 'esana',
-    alias: ['esananews','news'],
-    desc: 'whatsapp beta infomation.',
-    category: 'news',
-    react: "📃",
-    use: '<wbi>',
-  },
-        async(Void, citel, text) => {
 
-       try { const latst = await api.latest_id();
-            const nws = latst.results.news_id
-            let nn = text || nws
-            const ress = await api.news(nn);
-            const res = ress.results;
-
-            const txt2 = await Void.sendMessage(citel.chat, {image: {url: res.COVER}, caption: `\n*┣━( _📃ＥＳＥＮＡ📃ＮＥＷＳ📃_ )* \n\n*┃◉* *⇨ ᴛɪᴛᴇʟ :* ${res.TITLE}\n\n*┃◉* *⇨ ᴅᴀᴛᴇ :* ${res.PUBLISHED}\n\n*┃◉* *⇨ ᴜʀʟ :* ${res.URL}\n\n*┃◉* *⇨ Description :* ${res.DESCRIPTION}\n\n*┗━━━━━━━━━━━━━━◆*\n\nKing-ᴍᴅ ᴇꜱᴀɴᴀ ɴᴇᴡꜱ 📃\n\n©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ naveed`}, { quoted: citel });
-
-                await Void.sendMessage(citel.chat, { react: {
-        text: "📰",
-        key: txt2.key,
-            } } );
-
-    } catch (e) {
-    console.log(e)
-    citel.reply("❗ *" + e + "*")
-  }
-})
 
 //---------------------------------------------------------------------------
 cmd({
