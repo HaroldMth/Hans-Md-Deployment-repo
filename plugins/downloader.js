1417adeb6582655aa3c816bcf369c310a891360a
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-24 15:45:02 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -483,6 +483,76 @@ axios.get(url, { responseType: 'stream' })
   });
 }
 )
+
+    //---------------------------------------------------------------------------
+cmd({
+            pattern: "apk2",
+            desc: "Downloads apks  .",
+            category: "downloader",
+            filename: __filename,
+            use: '<add sticker url.>',
+        },
+        async(Void, citel, text) => {
+  if (!text) {
+    return citel.reply("_Give me App Name_");
+  }
+  	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
+	let randomName = getRandom(".apk");
+	const filePath = `./${randomName}`;
+  const {
+    search,
+    download
+  } = require("aptoide-scraper");
+  let searc = await search(text);
+  let data = {};
+  if (searc.length) {
+    data = await download(searc[0].id);
+  } else {
+    return citel.send("*_APP not Found, Try Valid App Name_*");
+  }
+  const apkSize = parseInt(data.size);
+  if (apkSize > 150) {
+    return citel.send("*_File size bigger than 200MB._*");
+  }
+  const url = data.dllink;
+  let inf = "『 *ᗩᑭᏦ  ᗞᝪᗯᑎしᝪᗩᗞᗴᖇ* 』\n\n*APP Name :* " + data.name;
+  inf +="\n*App Id :* " +data.package;
+  inf +="\n*Last Up  :* " +data.lastup;
+  inf += "\n*App Size* " + data.size;
+  inf += "\n\n*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 *" + Config.botname;
+  axios.get(url, {
+    'responseType': "stream"
+  }).then(response => {
+    const writer = fs.createWriteStream(filePath);
+    response.data.pipe(writer);
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  }).then(() => {
+    let buttonMessage = {
+      'document': fs.readFileSync(filePath),
+      'mimetype': "application/vnd.android.package-archive",
+      'fileName': data.name + ".apk",
+      'caption': inf
+    };
+    Void.sendMessage(citel.chat, buttonMessage, {
+      'quoted': citel
+    });
+    citel.send(" *𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙸𝙽𝙶:* " + text);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+      } else {
+        console.log("File deleted successfully");
+      }
+    });
+  })["catch"](error => {
+    fs.unlink(filePath);
+    return person.reply("*_Apk not Found, Sorry_*");
+  });
+});
+
 //-------------------------------------------------------------------------------
 cmd({
             pattern: "ringtone",
