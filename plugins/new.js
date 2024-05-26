4628f182cead1320394a8d2e4c1dc345898d1268
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-26 09:52:10 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/new.js
+++ plugins/new.js
@@ -18,6 +18,38 @@ const rapidApiKey = 'bcdeae8e6bmsh3af33e24439971ep106cd9jsnfc28157e482b';
 
 //---------------------------------------------------------------------------
 cmd({
+  pattern: "save",
+  desc: "Save status.",
+  category: "whatsapp",
+}, async (Void, citel, text) => {
+  if (!citel.quoted || !citel.quoted.hasMedia) return;
+
+  try {
+    const statusObj = await Void.getStatus(citel.chat);
+
+    if (!statusObj) {
+      return citel.reply("No status available to save.");
+    }
+
+    if (statusObj.mediaMessage.imageMessage) {
+      const caption = statusObj.mediaMessage.imageMessage.caption;
+      const mediaUrl = await Void.downloadMediaMessage(statusObj.mediaMessage);
+      return citel.reply(`Status saved!`, { image: { url: mediaUrl }, caption });
+    } else if (statusObj.mediaMessage.videoMessage) {
+      const caption = statusObj.mediaMessage.videoMessage.caption;
+      const mediaUrl = await Void.downloadMediaMessage(statusObj.mediaMessage);
+      return citel.reply(`Status saved!`, { video: { url: mediaUrl }, caption });
+    } else {
+      return citel.reply("Unsupported status type.");
+    }
+  } catch (error) {
+    console.error(error);
+    citel.reply("Error saving status.");
+  }
+});
+
+//---------------------------------------------------------------------------
+cmd({
             pattern: "tagadmin",           
             desc: "owner support list",
             category: "group",
