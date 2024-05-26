00fdc1eb3a56d8ae980f4e3d7a7cf99f1873b19b
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-26 21:55:18 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/search.js
+++ plugins/search.js
@@ -15,7 +15,7 @@
  **/
 
 const moment = require('moment-timezone')
-const {fetchJson,cmd, tlang } = require('../lib')
+const {fetchJson,cmd, tlang, getBuffer, prefix, Config } = require('../lib')
 let gis = require("async-g-i-s");
 const axios = require('axios')
 const fetch = require('node-fetch')
@@ -154,30 +154,51 @@ cmd({
     }
 )
     //---------------------------------------------------------------------------
+   const { shazam } = require('../lib')
+   let yts = require("secktor-pack");
+
 cmd({
-            pattern: "image",
-            alias :['img','imgsearch'],
-            category: "search",
-            desc: "Searches Image on Google",
-            use: '<text>',
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply("Provide me a query!")
-            if (!text) return reply("Hey bie please tell me for which pic you're looking");
-            let name1 = text.split("|")[0]
-            let name2 = text.split("|")[5] || `5`
-            citel.reply(`Sending ${name2} image(s) of ${name1} in chat`)
-            let nn = name2
-            for (let i = 0; i < nn; i++) {
+           pattern: "find",
+           category: "misc",
+           desc: "Finds info about song",
+           filename: __filename,
+       },
+       async(Void, citel, text) => {
+            let mime = citel.quoted.mtype
+            if (!citel.quoted) return citel.reply(`Send/Reply audio  ${prefix}find`);
+            if (!/audio/.test(mime)) return citel.reply(`Send/Reply audio ${prefix}shazam`);
+            let buff = await citel.quoted.download();
+            let data = await shazam(buff);
+            if (!data.status) return citel.send(data);
+            let search = await yts(data.title);
+            let anu = search.videos[0];
+            let h =`*Title : _${data.title}_*           
+*Artist : _${data.artists}_*            
+*To Download Song:- _${prefix}yta ${anu.url}_*
+   `
+//   *Album :* _${data.album}_    
+//   *Release :* _${data.release_date}
 
-                let n = await gis(name1)
-                    images = n[Math.floor(Math.random() * n.length)].url;
-                    await Void.sendMessage(citel.chat, {image: {  url: images,}, caption: `_King-Md Image Search_\n*${name1}*`,}, { quoted: citel, });
-            }
-        }
+
+   let buttonMessaged = {
+                   image: { url: anu.thumbnail, },
+                   caption: h,
+                   footer: tlang().footer,
+                   headerType: 4,
+                   contextInfo: {
+                       externalAdReply: {
+                           title: data.artists,
+                           body: data.album,
+                           thumbnail: log0,
+                           mediaType: 2,
+                           mediaUrl: ``,
+                           sourceUrl: ``,
+                       },
+                   },
+               };
+               await Void.sendMessage(citel.chat, buttonMessaged, { quoted: citel, });
+       }
     )
-
     //---------------------------------------------------------------------------
 cmd({
    pattern: "github",
@@ -383,23 +404,88 @@ catch (err) { return citel.reply("```Error While Fetching Snapshot```")}
 )
 
 //---------------------------------------------------------------------------
-     cmd({
-        pattern: 'screenshot',
-        alias :['webshot' , 'fullshot'],
-        category: "search",
-        desc: "Provides screenshot of any web",
-        use: '<text>',
-        filename: __filename,
-    },
-    async(Void, citel, text) => {
-let limit = 5;
-try {
-if (!text) return citel.reply("```Uhh Please, Give me Url!```");
-let urll = `https://image.thum.io/get/fullpage/=${text.match(/\bhttps?:\/\/\S+/gi)[0]}&width=1280&height=720`
-let media  = await getBuffer(urll)
-return await Void.sendMessage(citel.chat ,{image : media } , {quoted:citel} )
-}
-catch (err) { return citel.reply("```Error While Fetching Snapshot```")}
-    }
-)
+cmd({
+            pattern: "image",
+            alias: ["img" , "pic"],
+            category: "search",
+            desc: "Searches Image on Google",
+            use: '<text>',
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
 
+   if (!text) return citel.reply(`Provide me a query!\n*Ex : .image luffy |10*`)
+   let buttonMessage = {}
+   let name1 = text.split("|")[0] || `Luffy`
+   let name2 = text.split("|")[1] || `5`
+ try {
+    let urlsArray = [];
+    const params = {
+        q: name1, 
+        tbm: "isch",
+        hl: "en",
+        gl: "in",
+        ijn: "0", 
+    };
+    const headers = {
+      "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+      "Accept-Encoding": "application/json",
+  };
+  
+    const res = await axios.get("https://www.google.com/search", { headers: headers, params: params });
+    let body = res.data;
+    body = body.slice(body.lastIndexOf("AF_initDataCallback"));
+    body = body.slice(body.indexOf("["));
+    body = body.slice(0, body.indexOf("</script>")-1);
+    body = body.slice(0, body.lastIndexOf(","));
+    
+    const img = JSON.parse(body);
+
+    const imgObjects = img[56][1][0][0][1][0];
+    for (let i = 0; i < name2; i++) {
+        if (imgObjects[i] && imgObjects[i][0][0]["444383007"][1]) {
+            let url = imgObjects[i][0][0]["444383007"][1][3][0]; // the url
+            urlsArray.push(url);
+        }
+    }
+
+for (let url of urlsArray) { Void.sendMessage(citel.chat , {image : {url : url} } )  }
+} 
+ catch (error) {   return citel.reply("*Google Images Not Working, Try it Later*"); }
+
+ 
+ 
+ /*
+ 
+let isImages = false;
+            let num = text.split("|")[1];
+ gis(name1, async(error, result) => { 
+if(result.length) 
+{
+ isImages = true;
+ citel.reply(`Sending images of ${name1} in chat`) 
+}
+else return citel.reply("*Google Images Not Working, Try it Later*");
+})
+     if(!isImages) return       
+            let nn = name2
+            for (let i = 0; i < nn; i++) {
+            gis(name1, async(error, result) => { 
+            n = result;
+            images = n[Math.floor(Math.random() * n.length)].url;
+            
+             
+             if(!num){ buttonMessage = {   image: { url: images },
+                                caption: Config.caption,
+                                }
+             }else {  buttonMessage = {   image: { url: images },}   }
+                    
+             
+             Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
+                })
+            }
+            
+            
+            */
+ })
