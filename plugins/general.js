08708d172b053da74911c0faae4a44ad169208d9
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2024-05-28 22:55:22 +0500

    ✮ ᴋɪɴɢ-ᴍᴅ-ᴜᴘᴅᴀᴛᴇ ✮

--- plugins/general.js
+++ plugins/general.js
@@ -14,13 +14,10 @@
  ========================================================
  **/
 
-const { formatp, addnote, sck1, delnote, allnotes, delallnote,fetchJson, formatDate, performance, tlang, botpic, cmd, prefix, runtime, Config , parsedJid, sleep } = require('../lib')
+const { formatp, formatDate, performance, tlang, botpic, cmd, prefix, runtime, Config , parsedJid, sleep } = require('../lib')
 const axios = require('axios')
 const speed = require('performance-now')
 const fetch = require('node-fetch');
-const {TelegraPh} = require('../lib/scraper')
-const util = require('util');
-const fs = require('fs-extra');
 const API_KEY = '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'
 //---------------------------------------------------------------------------
       async function getDateTime() {
@@ -204,139 +201,6 @@ let buttonMessage = {
 Void.sendMessage(citel.chat,{image:{url:data.data[0].url}})
 }
 )
-
-    //---------------------------------------------------------------------------
-
-cmd({
-            pattern: "qr",
-            category: "user",
-            filename: __filename,
-            desc: "Sends CitelsVoid Qr code to scan and get your session id."
-        },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply(`*Provide me Text To Get QR*`);
-            let h =`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${text}`;
-            let buttonMessaged = 
-            {
-                image: { url: h },
-                caption: `*_Scan Qr To Get You Text_*`,
-                footer: ` Session bY >> ${Config.caption}`,
-                headerType: 4,
-            };
-            return await Void.sendMessage(citel.chat, buttonMessaged );
-         
-            let buttonMessaged = {
-                image: { url: 'https://king-md-session.onrender.com/qr' },
-                caption: `*_Scan Qr within 15 seconds_*\nYou'll get session id in your log number.`,
-                footer: ` Session bY >> KIng-Md`,
-                headerType: 4,
-                contextInfo: {
-                    externalAdReply: {
-                        title: 'mY bOT Session',
-                        body: 'Get you Session ID',
-                        thumbnail: log0,
-                        mediaType: 2,
-                        mediaUrl: ``,
-                        sourceUrl: gurl,
-                    },
-
-                },
-
-            };
-            await Void.sendMessage(citel.chat, buttonMessaged, {  quoted: citel });
-
-        })
-        
-//--------------------------------------------------------------------------- 
-cmd({
-    pattern: "rmbg",
-    alias : ['removebg'],
-    category: "AI",
-    filename: __filename,
-    desc: "Remove image Background."
-},
-async(Void, citel, text) => {
-  const _0x16b89c=_0x49c2;function _0x5e60(){const _0x33515f=['log','post','unlinkSync','send','caption','reply','524478FLbaqg','671116swmKkp','Uhh\x20Please,\x20Reply\x20To\x20An\x20Image/Video','1030BtYSxu','https://api.remove.bg/v1.0/removebg','187699OYwNUd','93599mPRWgy','auto','data','3341514nVKyCN','225184Osvztc','downloadAndSaveMediaMessage','catch','imageMessage','*_Uhh\x20Dear,\x20Please\x20provide\x20Valid\x20RemoveBg\x20Key_*\x0a\x0a\x20_Get\x20RemoveBg\x20api\x20key\x20from\x20remove.bg_\x0a\x20_then\x20Put\x20it\x20in\x20var\x20\x22REMOVE_BG_KEY\x22_\x0a\x0a','25NTBVPR','sending\x20removebg\x20image...!','14IYDGua','4141704bMSerR','REMOVE_BG_KEY','quoted','477ZxuHEt'];_0x5e60=function(){return _0x33515f;};return _0x5e60();}(function(_0x3045c0,_0x56c7b6){const _0x146dc7=_0x49c2,_0x492ef5=_0x3045c0();while(!![]){try{const _0x1f6d31=-parseInt(_0x146dc7(0xf0))/0x1*(-parseInt(_0x146dc7(0xe0))/0x2)+parseInt(_0x146dc7(0xd8))/0x3+-parseInt(_0x146dc7(0xec))/0x4+-parseInt(_0x146dc7(0xde))/0x5*(-parseInt(_0x146dc7(0xeb))/0x6)+parseInt(_0x146dc7(0xe1))/0x7+-parseInt(_0x146dc7(0xd9))/0x8*(parseInt(_0x146dc7(0xe4))/0x9)+parseInt(_0x146dc7(0xee))/0xa*(-parseInt(_0x146dc7(0xd5))/0xb);if(_0x1f6d31===_0x56c7b6)break;else _0x492ef5['push'](_0x492ef5['shift']());}catch(_0x1a9a50){_0x492ef5['push'](_0x492ef5['shift']());}}}(_0x5e60,0xe0b62));if(!citel[_0x16b89c(0xe3)])return await citel[_0x16b89c(0xea)]('*Reply\x20Any\x20Image\x20To\x20Remove\x20Background*');let mime=citel[_0x16b89c(0xe3)]['mtype'];if(mime!=_0x16b89c(0xdc))return await citel[_0x16b89c(0xea)](_0x16b89c(0xed));let media=await Void[_0x16b89c(0xda)](citel[_0x16b89c(0xe3)]),anu=await TelegraPh(media);try{await fs[_0x16b89c(0xe7)](media);}catch(_0x14f72d){}const formData={'image_url':anu,'size':_0x16b89c(0xd6)};function _0x49c2(_0x16a55d,_0x16a672){const _0x5e6049=_0x5e60();return _0x49c2=function(_0x49c2f0,_0x45b252){_0x49c2f0=_0x49c2f0-0xd5;let _0x43a2e2=_0x5e6049[_0x49c2f0];return _0x43a2e2;},_0x49c2(_0x16a55d,_0x16a672);}axios[_0x16b89c(0xe6)](_0x16b89c(0xef),formData,{'headers':{'X-Api-Key':Config[_0x16b89c(0xe2)]},'responseType':'arraybuffer'})['then'](_0x466f9f=>{const _0x441490=_0x16b89c;console[_0x441490(0xe5)](_0x441490(0xdf));const _0x5b34e5=Buffer['from'](_0x466f9f[_0x441490(0xd7)],'binary');return citel[_0x441490(0xe8)](_0x5b34e5,{'caption':Config[_0x441490(0xe9)]},'image');})[_0x16b89c(0xdb)](_0x46980f=>{const _0x3d5c75=_0x16b89c;return citel[_0x3d5c75(0xe8)](_0x3d5c75(0xdd)+Config[_0x3d5c75(0xe9)]);});
-
-    
-})
-
-//---------------------------------------------------------------------------
-
-cmd({
-            pattern: "url",
-            alias : ['createurl'],
-            category: "misc",
-            filename: __filename,
-            desc: "image to url."
-        },
-        async(Void, citel, text) => {
-            if (!citel.quoted) return await citel.reply(`*Reply To Any Image/Video To Get Url*`)
-            let mime = citel.quoted.mtype
-            if(mime !='videoMessage' && mime !='imageMessage' ) return await citel.reply("Uhh Please, Reply To An Image/Video")
-            let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
-            let anu = await TelegraPh(media);
-            await citel.reply(util.format(anu));
-            return await fs.unlinkSync(media);
-        })
-
-//---------------------------------------------------------------------------
-cmd({
-            pattern: "trt",
-            alias :['translate'],
-            category: "misc",
-            filename: __filename,
-            desc: "Translate\'s given text in desird language."
-        },
-        async(Void, citel, text) => {
-            if(!text && !citel.quoted) return await citel.reply(`*Please Give Me Text. Example: _${prefix}trt en Who are you_*`);
-            const translatte = require("translatte");
-            let lang = text ? text.split(" ")[0].toLowerCase() : 'en';
-            if (!citel.quoted)  { text = text.replace( lang , "");  }
-            else { text = citel.quoted.text; }
-            var whole = await translatte(text, { from:"auto",  to: lang , });
-            if ("text" in whole) { return await citel.reply(whole.text); }
-
-        }
-    )
-    //---------------------------------------------------------------------------
-
-
-cmd({
-            pattern: "shell",
-            category: "owner",
-            filename: __filename,
-            desc: "Runs command in Heroku(server) shell."
-        },
-        async(Void, citel, text,{ isCreator }) => {
-             if (!isCreator) return citel.reply(tlang().owner)
-             if(!text) return citel.reply("*Uhh PLease, Provide A Command to Run Heroku*")
-             const { exec } = require("child_process")
-             exec(text, (err, stdout) => {
-                     if (err) return citel.reply(`----${tlang().title}----\n\n` + err)
-                     if (stdout) { return citel.reply(`----${tlang().title}----\n\n` + stdout)  }
-             })
-        }) 
-        
-
-    //---------------------------------------------------------------------------
-
-
-cmd({
-            pattern: "eval",
-            category: "owner",
-            filename: __filename,
-            desc: "Runs js code on node server."
-        },
-        async(Void, citel, text,{ isCreator }) => {
-               if (!isCreator)  return citel.reply(tlang().owner)
-               if(!text) return citel.reply("*Uhh PLease, Provide A Query To Run Master*")
-               try {
-                   let resultTest = eval('const a = async()=>{\n' + text + '\n}\na()');
-                   if (typeof resultTest === "object") await citel.reply(JSON.stringify(resultTest));
-                   else await citel.reply(resultTest.toString());
-               } catch (err) { return  await citel.reply(err.toString()); }
-})
   
 
 //---------------------------------------------------------------------------
@@ -375,7 +239,7 @@ cmd({
   */
   //---------------------------------------------------------------------------
 
-if(Config.WORKTYPE != 'private')
+/**if(Config.WORKTYPE != 'private')
 {
 cmd({
             pattern: "ban",
@@ -404,9 +268,9 @@ cmd({
             } catch (e) {  return citel.reply("*Please Reply/Mention Any User.❌*")  }
 
 
-       })
+       })**/
      //---------------------------------------------------------------------------
-cmd({
+/**cmd({
             pattern: "unban",
             category: "misc",
             filename: __filename,
@@ -428,7 +292,7 @@ cmd({
                 })
             } catch {  return citel.reply("Please mention any user.❌");  }
         })
-}
+}**/
     //---------------------------------------------------------------------------
 //                  ADD NOTE  COMMANDS
 //---------------------------------------------------------------------------
@@ -451,7 +315,7 @@ cmd({
     )
  */
     //---------------------------------------------------------------------------
-cmd({
+/**cmd({
         pattern: "notes",
         alias : ['note'],
         category: "owner",
@@ -519,7 +383,7 @@ let txt = `╭───── *『 MONGODB NOTES 』* ───◆
  }
  else { return await citel.reply(txt) ; }
 
-})
+})**/
 
 //---------------------------------------------------------------------------
 cmd({
